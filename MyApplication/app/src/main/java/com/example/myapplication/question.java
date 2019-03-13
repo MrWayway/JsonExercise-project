62e83a3ca4493b8c0851e232e015087a74186ea2
MrWayway <33680090+MrWayway@users.noreply.github.com>
2019-03-13 12:40:20 +0700

    Bản nộp tạm do mất điện

--- MyApplication/app/src/main/java/com/example/myapplication/question.java
+++ MyApplication/app/src/main/java/com/example/myapplication/question.java
@@ -1,15 +1,4 @@
 package com.example.myapplication;
-
-import android.content.Context;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 public class question {
     public
     String head,tail,type;
@@ -22,17 +11,7 @@ public class question {
 
         return ;
     }
-    public String readText(Context context, int resId) throws IOException {
-        InputStream is = context.getResources().openRawResource(resId);
-        BufferedReader br= new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb= new StringBuilder();
-        String s= null;
-        while((  s = br.readLine())!=null) {
-            sb.append(s);
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
+
     public String toString()
     {
         StringBuilder ret= new StringBuilder();
