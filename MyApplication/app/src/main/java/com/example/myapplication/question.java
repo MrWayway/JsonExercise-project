ed262063c97ac647b79513327099d6051a6496bc
MrWayway <33680090+MrWayway@users.noreply.github.com>
2019-03-13 11:55:13 +0700

    Tempt version before display
    This is a temporary version and may contain bug.

--- MyApplication/app/src/main/java/com/example/myapplication/question.java
+++ MyApplication/app/src/main/java/com/example/myapplication/question.java
@@ -2,6 +2,9 @@ package com.example.myapplication;
 
 import android.content.Context;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,10 +13,14 @@ import java.io.InputStreamReader;
 public class question {
     public
     String head,tail,type;
-    public question readfile(Context content ) throws IOException
+    public question readfile(Context content ) throws IOException, JSONException
     {
         question ret = new question();
         String a = readText(content,R.raw.my);
+        JSONObject tempt = new JSONObject(a);
+        ret.head = tempt.getString("head");
+        ret.tail = tempt.getString("tail");
+        ret.type = tempt.getString("type");
 
         return ret;
     }
@@ -27,5 +34,16 @@ public class question {
             sb.append("\n");
         }
         return sb.toString();
+    }
+    public String toString()
+    {
+        StringBuilder ret= new StringBuilder();
+        ret.append("Head:");
+        ret.append(this.head);
+        ret.append("\nTail:");
+        ret.append(this.tail);
+        ret.append("\nType:");
+        ret.append(this.type);
+        return ret.toString();
     }
 }
