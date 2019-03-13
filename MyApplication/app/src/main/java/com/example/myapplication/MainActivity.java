7b16f67b351cccae90b4448f31126b7ef839161a
MrWayway <33680090+MrWayway@users.noreply.github.com>
2019-03-13 10:59:45 +0700

    simple version
    rewrite everything from scratch

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView dis;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dis= (TextView)this.findViewById(R.id.textView);
+        runExample();
+    }
+    public String readText(Context context, int resId) throws IOException {
+        InputStream is = context.getResources().openRawResource(resId);
+        BufferedReader br= new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb= new StringBuilder();
+        String s= null;
+        while((  s = br.readLine())!=null) {
+            sb.append(s);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+    public void runExample()  {
+        try {
+            dis.setText(readText(this,R.raw.my));
+        } catch(Exception e)  {
+            dis.setText(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/Read.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Read {
+
+
+}
